fe3bc226f4c2f5d1d586236f1ab1fc6e5e87e7eb
ajoshi-CB <ajoshi@cloudbees.com>
2025-09-01 09:54:59 +0530

    part2: Level 7 part b done. Testing is in progress.

---
+++ src/main/java/io/jenkins/plugins/sample/OnboardingStep.java
@@ -0,0 +1,63 @@
+package io.jenkins.plugins.sample;
+
+import hudson.Extension;
+import hudson.model.TaskListener;
+import hudson.util.ListBoxModel;
+import java.util.Collections;
+import java.util.Set;
+import jenkins.model.GlobalConfiguration;
+import org.jenkinsci.Symbol;
+import org.jenkinsci.plugins.workflow.steps.*;
+import org.kohsuke.stapler.DataBoundConstructor;
+
+public class OnboardingStep extends Step {
+
+    private final String category;
+
+    @DataBoundConstructor
+    public OnboardingStep(String category) {
+        System.out.println("Inside OnboardingStep constructor");
+        this.category = category;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    @Override
+    public StepExecution start(StepContext context) throws Exception {
+        System.out.println("Starting OnboardingStep execution with category: " + category);
+        return new OnboardingStepExecution(this, context);
+    }
+
+    @Extension
+    @Symbol("onboardingStep")
+    public static class DescriptorImpl extends StepDescriptor {
+
+        @Override
+        public Set<? extends Class<?>> getRequiredContext() {
+            return Collections.singleton(TaskListener.class);
+        }
+
+        @Override
+        public String getFunctionName() {
+            return "onboardingStep";
+        }
+
+        @Override
+        public String getDisplayName() {
+            return "Onboarding Step";
+        }
+
+        public ListBoxModel doFillCategoryItems() {
+            ListBoxModel items = new ListBoxModel();
+            OnboardingConfiguration globalConfig = GlobalConfiguration.all().get(OnboardingConfiguration.class);
+            if (globalConfig != null) {
+                for (ListEntry category : globalConfig.getEntries()) {
+                    items.add(category.getName(), category.getName());
+                }
+            }
+            return items;
+        }
+    }
+}
